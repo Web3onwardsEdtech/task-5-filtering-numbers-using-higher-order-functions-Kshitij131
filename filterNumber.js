fafaa3d82178e8ffd69aba2a4eb28319863a0bb5
Kshitij131 <1ms23cy032@msrit.edu>
2025-04-05 01:28:28 +0530

    Completed task 3

--- filterNumber.js
+++ filterNumber.js
@@ -1,1 +1,21 @@
+function filterNumbers(arr, criterion) {
+    switch (criterion) {
+        case 'even':
+            return arr.filter(num => num % 2 === 0);
+        case 'odd':
+            return arr.filter(num => num % 2 !== 0);
+        case 'positive':
+            return arr.filter(num => num > 0);
+        case 'negative':
+            return arr.filter(num => num < 0);
+        default:
+            return arr;
+    }
+}
 
+// Example Usage:
+console.log(filterNumbers([1, 2, 3, 4, 5, 6], 'even'));           // [2, 4, 6]
+console.log(filterNumbers([1, 2, 3, 4, 5, 6], 'odd'));            // [1, 3, 5]
+console.log(filterNumbers([-1, -2, 3, 4, -5, 6], 'negative'));    // [-1, -2, -5]
+console.log(filterNumbers([0, 1, -3, 5, -6], 'positive'));        // [1, 5]
+console.log(filterNumbers([1, -2, 3, -4, 5, -6], ''));            // [1, -2, 3, -4, 5, -6]
